3da44b09c9df09171d61d50b34f103b76611ae26
Roberio Rocha <roberiorocha@gmail.com>
2022-10-11 18:51:43 -0300

    iniciando node.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
+import { stringify } from "querystring";
 
 const app = express();
 
@@ -9,9 +10,45 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const characters = [
+  {
+    id: 1,
+    name: "Personagem 1"
+  },
+  {
+    id: 2,
+    name: "Personagem 2"
+  },
+  {
+    id: 3,
+    name: "Personagem 3"
+  }
+]
+
 app.get('/', (req, res) => {
   res.json({
     test: "a"
+  })
+})
+
+app.get('/characters', (req, res) => {
+  res.json({
+    characters
+  })
+})
+
+app.post('/characters', (req, res) => {
+  console.log(req.body);
+  res.json({
+    test: "a"
+  })
+})
+
+app.get('/characters/:id', async(req, res) => {
+  const {id} = req.params;
+  console.log("id", id);
+  res.json({
+    characters: characters.filter((character) => String(character.id) === String(id))
   })
 })
 
